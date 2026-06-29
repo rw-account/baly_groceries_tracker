fda9b74abf3b5e8269652c73d31d54c9def41fb6
rw-account <whybalnbhany@gmail.com>
2026-06-29 06:09:16 +0300

    wip

--- lib/screens/shopping_list/shopping_list_screen/widgets/shopping_list_view.dart
+++ lib/screens/shopping_list/shopping_list_screen/widgets/shopping_list_view.dart
@@ -1,26 +1,26 @@
 // lib/screens/shopping_list/shopping_list_screen/widgets/shopping_list_view.dart
 
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../../models/shopping_item_model.dart';
-import '../../../../providers/shopping_list_provider.dart';
 import '../../../../core/widgets/shopping_item_card.dart';
 import 'shopping_list_total_bar.dart';
 
 /// Renders the list of shopping items plus the running total bar.
 /// Used for the "data" case of [shoppingListProvider] once the list
 /// is known to be non-empty.
-class ShoppingListView extends ConsumerWidget {
+class ShoppingListView extends StatelessWidget {
   const ShoppingListView({
     super.key,
     required this.items,
+    required this.onDelete,
   });
 
   final List<ShoppingItem> items;
+  final ValueChanged<ShoppingItem> onDelete;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final total = items.fold<double>(
       0,
       (sum, item) => sum + (item.price ?? 0),
@@ -38,23 +38,7 @@ class ShoppingListView extends ConsumerWidget {
                 padding: const EdgeInsets.only(bottom: 10),
                 child: ShoppingItemCard(
                   item: item,
-                  onDelete: () {
-                    if (item.id != null) {
-                      ref
-                          .read(shoppingListProvider.notifier)
-                          .deleteShoppingItem(item.id!);
-                    }
-                  },
-                  onUndo: (deletedItem) {
-                    // إعادة إضافة العنصر المحذوف
-                    ref.read(shoppingListProvider.notifier).addShoppingItem(
-                      title: deletedItem.title,
-                      inventoryItemId: deletedItem.inventoryItemId,
-                      price: deletedItem.price,
-                      isChecked: deletedItem.isChecked,
-                      createdAt: deletedItem.createdAt,
-                    );
-                  },
+                  onDelete: onDelete,
                 ),
               );
             },
