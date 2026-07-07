7101edace5de04b33dc31f067683c60bdc6d2288
rw-account <whybalnbhany@gmail.com>
2026-07-07 11:05:40 +0300

    wip

--- lib/screens/home/widgets/home_app_bar.dart
+++ lib/screens/home/widgets/home_app_bar.dart
@@ -1,15 +1,16 @@
 // lib/screens/home/widgets/home_app_bar.dart
 import 'package:flutter/material.dart';
-import 'summary_badge.dart';
 
 class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
   final int urgentCount;
   final int warningCount;
+  final VoidCallback onSharePressed;
 
   const HomeAppBar({
     super.key,
     required this.urgentCount,
     required this.warningCount,
+    required this.onSharePressed,
   });
 
   @override
@@ -18,15 +19,28 @@ class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
       title: const Text('متابعة طلبات البيت'),
       centerTitle: false,
       actions: [
-          if (urgentCount > 0 || warningCount > 0)
-            Padding(
-              padding: const EdgeInsets.only(left: 8, right: 4),
-              child: SummaryBadge(
-                urgentCount: urgentCount,
-                warningCount: warningCount,
+        PopupMenuButton<String>(
+          icon: const Icon(Icons.more_vert),
+          position: PopupMenuPosition.under,
+          onSelected: (value) {
+            if (value == 'share') {
+              onSharePressed();
+            }
+          },
+          itemBuilder: (context) => [
+            const PopupMenuItem(
+              value: 'share',
+              child: Row(
+                children: [
+                  Icon(Icons.share_outlined, size: 20),
+                  SizedBox(width: 8),
+                  Text('مشاركة تفاصيل المواد'),
+                ],
               ),
             ),
-            ],
+          ],
+        ),
+      ],
     );
   }
 
