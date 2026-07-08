6d1823580918b299b77198c3c9b1f696a797fb58
rw-account <whybalnbhany@gmail.com>
2026-07-08 14:17:48 +0300

    wip

--- lib/screens/home/widgets/widgets.dart
+++ lib/screens/home/widgets/widgets.dart
@@ -1,8 +1,5 @@
 // lib/screens/home/widgets/widgets.dart
 
-// ملف تجميعي (barrel file): يسمح باستيراد كل ودجِتس الشاشة بسطر واحد:
-//   import 'widgets/widgets.dart';
-
 export 'empty_state.dart';
 export 'home_app_bar.dart';
 export 'items_list.dart';
