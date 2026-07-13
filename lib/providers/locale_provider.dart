bbb0fc285efd77b62cfa407bdf998c77cbff941e
rw-account <whybalnbhany@gmail.com>
2026-07-13 07:31:25 +0300

    wip

--- lib/providers/locale_provider.dart
+++ lib/providers/locale_provider.dart
@@ -6,19 +6,21 @@ part 'locale_provider.g.dart';
 
 @riverpod
 class LocaleNotifier extends _$LocaleNotifier {
+
+  static String currentLanguage = 'ar';
+
   @override
   Future<Locale> build() async {
     final prefs = await SharedPreferences.getInstance();
     final languageCode = prefs.getString('language_code') ?? 'ar';
+    currentLanguage = languageCode;
     return Locale(languageCode);
   }
 
-  // دالة لتغيير اللغة وحفظها فوراً
   Future<void> changeLocale(String languageCode) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setString('language_code', languageCode);
-    
-    // تحديث الحالة ليراها التطبيق فوراً
+    await prefs.setString('language_code', languageCode);   
+    currentLanguage = languageCode;
     state = AsyncData(Locale(languageCode));
   }
 }
