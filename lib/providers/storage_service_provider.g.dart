beb4a86245102c4072c15076de6c505d30c8dcd0
rw-account <whybalnbhany@gmail.com>
2026-07-16 17:25:52 +0300

    wip

---
+++ lib/providers/storage_service_provider.g.dart
@@ -0,0 +1,51 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'storage_service_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(storageService)
+final storageServiceProvider = StorageServiceProvider._();
+
+final class StorageServiceProvider
+    extends $FunctionalProvider<StorageService, StorageService, StorageService>
+    with $Provider<StorageService> {
+  StorageServiceProvider._()
+      : super(
+          from: null,
+          argument: null,
+          retry: null,
+          name: r'storageServiceProvider',
+          isAutoDispose: true,
+          dependencies: null,
+          $allTransitiveDependencies: null,
+        );
+
+  @override
+  String debugGetCreateSourceHash() => _$storageServiceHash();
+
+  @$internal
+  @override
+  $ProviderElement<StorageService> $createElement($ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  StorageService create(Ref ref) {
+    return storageService(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(StorageService value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<StorageService>(value),
+    );
+  }
+}
+
+String _$storageServiceHash() => r'a6d23bc030486b6d1106efa40d3a7733b6bf906f';
