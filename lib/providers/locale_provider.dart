53de6db5f731a5039b4ccbcadc8872b1a25c3e04
rw-account <whybalnbhany@gmail.com>
2026-07-17 22:57:50 +0300

    wip

--- lib/providers/locale_provider.dart
+++ lib/providers/locale_provider.dart
@@ -1,9 +1,9 @@
 // lib/providers/locale_provider.dart
 
 import 'package:flutter/material.dart';
+import 'package:home_orders_tracker/providers/app_state.dart';
 import 'package:home_orders_tracker/providers/storage_service_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import '../services/notification_service.dart';
 
 part 'locale_provider.g.dart';
@@ -13,7 +13,7 @@ class LocaleNotifier extends _$LocaleNotifier {
   static String currentLanguage = 'ar';
 
   @override
-  Future<Locale> build() async {
+  Locale build() {
     final appState = ref.read(appStateNotifierProvider);
     final languageCode = appState.prefs.getString('language_code') ?? 'ar';
     currentLanguage = languageCode;
@@ -22,18 +22,17 @@ class LocaleNotifier extends _$LocaleNotifier {
 
   Future<void> changeLocale(String languageCode) async {
     final appState = ref.read(appStateNotifierProvider);
-    await appState.prefs.setString('language_code', languageCode);   
+    await appState.prefs.setString('language_code', languageCode);
+
     currentLanguage = languageCode;
-    state = AsyncData(Locale(languageCode));
+    state = Locale(languageCode);
 
     // Reschedule the daily summary notification to apply the new language,
     // ensuring the alert is displayed in the user's updated locale.
     try {
       final storage = ref.read(storageServiceProvider);
       final items = await storage.getAllItems();
-
       await NotificationService.scheduleDailySummary(items);
-    } catch (_) {
-    }
+    } catch (_) {}
   }
 }
